dd5ad0dc559fefe50cf0a95125ab2e69b16b5063
antonioc83 <ntn.cesari@gmail.com>
2023-04-27 12:05:56 +0200

    section clients

--- src/components/section_clients_business.jsx
+++ src/components/section_clients_business.jsx
@@ -5,16 +5,21 @@ export default function SectionClientBusiness() {
     <section className="business-clients">
       <div className="business-clients--container container">
         <div className="business-clients--content">
-        <h3 className="business-clients--title">Scopri come i nostri clienti hanno sbloccato il potenziale dell'open &amp; social learning con Viblio</h3>
-        <div className="business-clients--sub-content">
-        <h4>Utente 1</h4>
-        </div>
-        <div className="business-clients--sub-content">
-        <h4>Utente 2</h4>
-        </div>
-        <div className="business-clients--sub-content">
-        <h4>Utente 3</h4>
-        </div>
+          <h3 className="business-clients--title">Scopri come i nostri clienti hanno sbloccato il potenziale dell'open &amp; social learning con Viblio</h3>
+          <div className="business-clients--contain-sub-content">
+            <div className="business-clients--sub-content">
+              <h4>Utente 1</h4>
+              <p>Qui ci va una sbrodolata di testo per riempire</p>
+            </div>
+            <div className="business-clients--sub-content">
+              <h4>Utente 2</h4>
+              <p>Qui ci va una sbrodolata di testo per riempire</p>
+            </div>
+            <div className="business-clients--sub-content">
+              <h4>Utente 3</h4>
+              <p>Qui ci va una sbrodolata di testo per riempire</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
