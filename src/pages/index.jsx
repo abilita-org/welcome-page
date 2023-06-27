2737d02090ee3e0b746d05fe3622c60e65788410
antonioc83 <ntn.cesari@gmail.com>
2023-06-27 16:48:16 +0200

    d

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ import SectionCover from "../components/section_cover"
 import SectionFeature from "../components/section_feature"
 import SectionPartner from "../components/section_partner"
 import SectionClients from "../components/section_clients"
-import { withI18next } from 'gatsby-plugin-i18next';
+import { withI18next } from 'gatsby-plugin-react-i18next';
 
 import "../styles/style.scss"
 
