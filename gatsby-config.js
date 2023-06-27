747566302b4a259369efa6676dd4d43cdd7768cf
antonioc83 <ntn.cesari@gmail.com>
2023-06-27 14:43:04 +0200

    language package

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,24 @@ module.exports = {
       __key: "images",
     },
     "gatsby-plugin-sass",
+    {
+    resolve: `gatsby-plugin-react-i18next`,
+    options: {
+      localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+      languages: [`en`, `es`, `de`],
+      defaultLanguage: `en`,
+      siteUrl: `https://example.com`,
+      // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
+      trailingSlash: 'always',
+      // you can pass any i18next options
+      i18nextOptions: {
+        interpolation: {
+          escapeValue: false // not needed for react as it escapes by default
+        },
+        keySeparator: false,
+        nsSeparator: false
+      },
+    }
+  },
   ],
 }
